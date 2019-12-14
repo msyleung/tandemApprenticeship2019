dc4658ceb198df7d9aab35f83195a766e10caf15
Meo Leung <misyleung@gmail.com>
2019-12-14 14:22:15 -0800

    [UPD] more env changes to resolve H10 code error

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,15 @@
-ENV['SINATRA_ENV'] ||= "development"
 require 'bundler'
-
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
- )
+configure :development do
+  ENV['SINATRA_ENV'] ||= "development"
+  # require 'bundler/setup'
+  # Bundler.require(:default, ENV['SINATRA_ENV'])
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  )
 end
 
 configure :production do
