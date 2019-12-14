858f9d3a27fb423ce900d2b7196528e85357e00c
Meo Leung <misyleung@gmail.com>
2019-12-14 14:05:46 -0800

    [UPD] fixed config env for production

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,13 @@ require 'bundler'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(
+  if ENV['SINATRA_ENV'].production?
+    :adapter => "postgresql",
+    :database => "db/plant_#{ENV['SINATRA_ENV']}"
+  else
   :adapter => "sqlite3",
   :database => "db/plant_#{ENV['SINATRA_ENV']}.sqlite"
+  end
 )
 
 ActiveRecord::Base.logger = Logger.new(STDOUT)
